e4a342ceb634bb4425a6ab326369c4889d36bb8c
Subhadip Banerjee <subhadipbanerjee.527@gmail.com>
2018-12-13 20:50:22 +0530

    Update check.py
    loop added

--- check.py
+++ check.py
@@ -2,23 +2,23 @@ import requests as rq
 import json
 import getpass
 import xmltodict
+import time
 
 
-username = "subhadipbanerjee527"	#input("username: ")
-password = "Subhadip7679680429"		#getpass.getpass("Password")
+username = #input("username: ")
+password = #getpass.getpass("Password")
 
 
 
 def checkAndBreak(mailsub):
-	res = rq.get("https://mail.google.com/mail/feed/atom",auth=(username, password))
+	while True:
+		res = rq.get("https://mail.google.com/mail/feed/atom",auth=(username, password))
+		emailjson=json.loads(json.dumps(xmltodict.parse(res.text)))
+		emailsub = []
 
-
-	emailjson=json.loads(json.dumps(xmltodict.parse(res.text)))
-	emailsub = []
-
-	for entry in emailjson['feed']['entry']:
-		#emailsub.append(entry["title"])
-		if mailsub == entry["title"]:
-			exit(1)
-
+		for entry in emailjson['feed']['entry']:
+			#emailsub.append(entry["title"])
+			if mailsub == entry["title"]:
+				exit(1)
+		sleep(5)	
 
